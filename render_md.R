1bbb26c6c241ae128a5f5136ae066f71d2021ca2
Emanuele Aliverti <emanuele.aliverti@gmail.com>
2022-01-17 18:29:16 +0100

    bw

--- render_md.R
+++ render_md.R
@@ -3,4 +3,4 @@ render("ENDOMETRIAL/endometrial.Rmd", md_document(variant = "gfm"))
 system("cp ENDOMETRIAL/endometrial.md ENDOMETRIAL/README.md")
 
 render("BIRTHWEIGHT/birthweight.Rmd", md_document(variant = "gfm"))
-system("cp BIRTHWEIGHT/birthweight.Rmd BIRTHWEIGHT/README.md")
+system("cp BIRTHWEIGHT/birthweight.md BIRTHWEIGHT/README.md")
