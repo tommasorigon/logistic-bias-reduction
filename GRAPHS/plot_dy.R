9230813ce006e5adb86100b545a9b08416a0cb43
Tommaso Rigon <tommaso.rigon@gmail.com>
2022-01-18 17:10:52 +0100

    Create plot_dy.R

---
+++ GRAPHS/plot_dy.R
@@ -0,0 +1,50 @@
+rm(list = ls())
+set.seed(1991)
+x <- rnorm(5)
+X <- cbind(1, x)
+
+dy_prior <- function(beta1, beta2, X, kappai = rep(0.5, NROW(X)), tau = NCOL(X) / NROW(X)) {
+  nu <- X %*% c(beta1, beta2)
+  out <- tau * (t(kappai) %*% nu - sum(log(1 + exp(nu))))
+  return(exp(out))
+}
+dy_prior(0, 0, X = X)
+
+jeff_prior <- function(beta1, beta2, X) {
+  pis <- plogis(X %*% c(beta1, beta2))
+  w <- pis * (1 - pis)
+  XtX<- t(X) %*% diag(c(w), NROW(X), NROW(X)) %*% X
+  return(exp(0.5* determinant(XtX, logarithm = TRUE)$modulus))
+}
+
+rr <- seq(-4, 4, l = 200)
+gr <- gr2 <- expand.grid(rr, rr)
+
+gr$values <- apply(gr, 1, function(x) dy_prior(x[1], x[2], X = X))
+gr2$values <- apply(gr2, 1, function(x) jeff_prior(x[1], x[2], X = X))
+
+# Normalizzazione stupida
+library(cubature)
+
+C_dy <- hcubature(function(x) dy_prior(x[1], x[2], X = X),c(-Inf,-Inf), c(Inf,Inf))$integral
+C_jeff <- hcubature(function(x) jeff_prior(x[1], x[2], X = X),rep(-Inf,2), rep(Inf, 2))$integral
+
+gr$values <- gr$values / C_dy
+gr2$values <- gr2$values / C_jeff
+
+gr$w <- "Diaconis & Ylvisaker"
+gr2$w <- "Jeffrey"
+gr_c <- rbind(gr, gr2)
+
+library(ggplot2)
+library(metR)
+ggplot(gr_c) +
+  geom_contour(aes(x = Var1, y = Var2, z = values), bins = 12, col = "gray") +
+  geom_text_contour(aes(x = Var1, y = Var2, z = values)) +
+  facet_wrap(~w) +
+  theme_bw() + xlab(expression(beta[1])) + ylab(expression(beta[2]))
+
+ggplot(gr_c) +
+  geom_contour_filled(aes(x = Var1, y = Var2, z = values), bins = 30) +
+  facet_wrap(~w) +
+  theme_bw() + xlab(expression(beta[1])) + ylab(expression(beta[2])) + theme(legend.position = "none")
