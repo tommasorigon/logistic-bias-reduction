70a7dcb06de1211cfb9ac33674f0f8a64a16d6fd
Emanuele Aliverti <emanuele.aliverti@gmail.com>
2022-01-18 20:18:19 +0100

    first study

--- HIGH-DIMENSIONAL/evaluate-bias.r
+++ HIGH-DIMENSIONAL/evaluate-bias.r
@@ -1,19 +1,18 @@
 set.seed(1991)
-n <- 500
-p <- 100
+n <- 1000
+p <- 200
+beta <- c(rep(10, p/8), rep(-10, p/8), rep(0, 3*p/4))
+X <- matrix(rnorm(n * p, 0, sqrt(1/n)), n, p)
+y <- rbinom(n, 1, plogis(X %*% beta))
 
-SS <- matrix(0.2, p - 1, p - 1)
-diag(SS) <- 1
 
-X <- cbind(1, matrix(rnorm(n * (p - 1)), n, p - 1) %*% chol(SS))
-betas <- runif(p)/10
-y <- rbinom(n, 1, prob = plogis(X %*% betas))
+
 
 m0 <- glm(y ~ X - 1, family = binomial("logit"))
-Nsim = 1e4
+Nsim = 1e3
 y_sim = simulate(m0,nsim = Nsim)
 
-ml <- br <- mbr <- clogg <- dy <- matrix(NA, nrow = Nsim, ncol = length(betas))
+ml <- br <- clogg <- dy <- matrix(NA, nrow = Nsim, ncol = length(beta))
 
 library(brglm2)
 library(RcppNumerical)
@@ -38,15 +37,38 @@ for(i in 1:Nsim) {
                    method = "brglmFit", type = "AS_mean"
   )
 
-  # KENNE PAGUI ET AL. (2017)
-  fit_kp <- glm(y ~ -1 + X,
-                family = binomial("logit"),
-                method = "brglmFit", type = "AS_median"
-  )
+
 
   ml[i,] = coef(mle)
   dy[i,] = fit_fast_dy$coefficients
   clogg[i,] = fit_fast_clogg$coefficients
   br[i,] = coef(fit_firth)
-  mbr[i,] = coef(fit_kp)
 }
+
+
+## BIAS in beta parameterization
+bias.beta <- data.frame(
+  ml = colMeans(ml[ ]),
+  br = colMeans(br),
+  clogg = colMeans(clogg),
+  dy = colMeans(dy)
+) - beta
+
+## MSE in beta parameterization
+mse.beta <- data.frame(
+  ml = rowMeans((t(ml[, ]) - beta)^2),
+  br = rowMeans((t(br) - beta)^2),
+  clogg = rowMeans((t(clogg) - beta)^2),
+  dy = rowMeans((t(dy) - beta)^2)
+)
+bias.beta
+df = reshape2::melt(bias.beta)
+head(df)
+library(ggplot2)
+table(df$variable)
+ggplot(df) +
+  geom_boxplot(aes(y = value, color = variable, group = variable)) +
+  theme_bw()
+
+matplot(bias.beta)
+matplot(mse.beta)
