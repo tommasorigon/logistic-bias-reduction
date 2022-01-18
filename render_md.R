5216770d914e702f034cbb59ccc1135f85281704
Tommaso Rigon <tommaso.rigon@gmail.com>
2022-01-18 15:32:03 +0100

    Updates

--- render_md.R
+++ render_md.R
@@ -1,3 +1,4 @@
 library(rmarkdown)
 render("ENDOMETRIAL/endometrial.Rmd", md_document(variant = "gfm"),output_file = "README.md",output_dir = "ENDOMETRIAL")
 render("BIRTHWEIGHT/birthweight.Rmd", md_document(variant = "gfm"), output_file = "README.md", output_dir = "BIRTHWEIGHT")
+render("GRAPHS/graphs.Rmd", md_document(variant = "gfm"),output_file = "README.md",output_dir = "GRAPHS")
